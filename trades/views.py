678c9949397208573ecf7694ffb0510a4db39c10
Gabriel Lota <gabriellota4@gmail.com>
2026-06-05 10:33:27 +0200

    Create home view for trades app

--- trades/views.py
+++ trades/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Trade
+
 
 def home(request):
     trades = Trade.objects.all()
@@ -9,3 +10,9 @@ def home(request):
         'trades/home.html',
         {'trades': trades}
     )
+
+
+def delete_trade(request, trade_id):
+    trade = get_object_or_404(Trade, id=trade_id)
+    trade.delete()
+    return redirect('home')
