253eeae7bb5eda6295adcd836bea6f61bbc50f63
Gabriel Lota <gabriellota4@gmail.com>
2026-06-09 06:08:20 +0200

    Prepare project for Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tradecal_project.wsgi
--- tradecal_project/settings.py
+++ tradecal_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0s4(mhinz)&y(%jx)twd5a$19*vrl!ggsl2wi4jy9e+e@$^^bh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tradecal-gabriel-lota.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
